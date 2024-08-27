ec4bfae94ff94d1e5ede923a291f4b25195d5ddc
angelo-daniel <nastyandaniel8@gmail.com>
2024-08-27 16:48:55 +0800

    thisismyfirstcommit

---
+++ resources/views/daniel2.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1 class=" text-[30px] tracking-wider text-center text-yellow-500 text-xl font-bold lowercase">This is my demo 2</h1>
+    <button>Go back to demo 1</button>
+</body>
+</html>
+
+
---
+++ resources/views/mypages/gallery.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gallery</title>
+</head>
+<body>
+    <h1>This is gallery</h1>
+    <br>
+    <a href="{{ route('gallery') }}">
+        <button>Gallery</button>
+    </a>
+    <br>
+    <a href="{{ route('index') }}">
+        <button>Index</button>
+    </a>
+    <br>
+    <a href="{{ route('services') }}">
+        <button>Services</button>
+    </a>
+</body>
+</html>
